346fca51d9e0c82845ba483b08f00fd0df8aec95
zozokjs <36176836+zozokjs@users.noreply.github.com>
2022-05-16 18:05:16 +0900

    main commit test

--- library/src/main/java/com/kjs/library/LibraryStartApplication.java
+++ library/src/main/java/com/kjs/library/LibraryStartApplication.java
@@ -7,7 +7,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryStartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryStartApplication.class, args);
-		
-		
 	}
 }
