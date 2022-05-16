41efa45fa40e798be361bcea1203e68e1e83b7c4
zozokjs <36176836+zozokjs@users.noreply.github.com>
2022-05-16 17:24:35 +0900

    test2

---
+++ library/src/main/java/com/kjs/library/LibraryStartApplication.java
@@ -0,0 +1,13 @@
+package com.kjs.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryStartApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryStartApplication.class, args);
+		
+		
+	}
+}
