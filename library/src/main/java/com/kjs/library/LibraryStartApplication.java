f25af18dfbf00290bcf3e5ccf9f0ec9e9784a21e
zozokjs <36176836+zozokjs@users.noreply.github.com>
2022-05-16 18:10:31 +0900

    브랜치 문제 해결됨

--- library/src/main/java/com/kjs/library/LibraryStartApplication.java
+++ library/src/main/java/com/kjs/library/LibraryStartApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryStartApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryStartApplication.class, args);
+		
 	}
 }
