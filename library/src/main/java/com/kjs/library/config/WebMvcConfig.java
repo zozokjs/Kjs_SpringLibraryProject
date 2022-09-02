b4292b27d35439e5f69ea6c9b7bc542a1b645aeb
zozokjs <36176836+zozokjs@users.noreply.github.com>
2022-09-02 13:16:31 +0900

    주석 위치 변경

--- library/src/main/java/com/kjs/library/config/WebMvcConfig.java
+++ library/src/main/java/com/kjs/library/config/WebMvcConfig.java
@@ -23,11 +23,8 @@ public class WebMvcConfig implements WebMvcConfigurer{
 		registry.addResourceHandler("/upload/**")
 		//명시한 URL 패턴에 일치하는 요청이 발생하면 이 핸들러가 동작함.
 		//JSP 페이지에서 /upload/~~이런 패턴이 나오면 발동됨.
-		.addResourceLocations("file:///"+upload_imageTitleFolder)
-		//이 메소드로 정적 콘텐츠를 제공할 리소스 위치를 추가함.
-		.setCachePeriod(3600)
-		//이미지를 빨리 호출하기 위해 브라우저가 이미지를 HDD에 임시 저장하는 걸 캐싱이라고 하는데
-		//이 캐싱 기간을 설정함
+		.addResourceLocations("file:///"+upload_imageTitleFolder) //이 메소드로 정적 콘텐츠를 제공할 리소스 위치를 추가함.
+		.setCachePeriod(3600)	//이미지를 빨리 호출하기 위해 브라우저가 이미지를 HDD에 임시 저장하는 걸 캐싱이라고 하는데 이 캐싱 기간을 설정함
 		.resourceChain(true) //이것을 false로 한다면, 요청할때마다 브라우저가 자원을 새로 다운로드함
 		.addResolver(new PathResourceResolver());
 	
